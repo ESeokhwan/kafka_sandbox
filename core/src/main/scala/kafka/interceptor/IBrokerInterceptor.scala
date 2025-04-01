a18544f587c7edd89e873fcd69432ecc33f2bf02
eseokhwan <huan99@hanmail.net>
2025-04-01 17:54:14 +0900

    chore: change order of methods in IBrokerInterceptor

--- core/src/main/scala/kafka/interceptor/IBrokerInterceptor.scala
+++ core/src/main/scala/kafka/interceptor/IBrokerInterceptor.scala
@@ -3,13 +3,13 @@ package kafka.interceptor
 import kafka.network.RequestChannel
 
 trait IBrokerInterceptor {
+  def beforeSendRequestToQueue(request: RequestChannel.Request, connectionId: String): Unit
+
+  def afterUnmuteChannel(response: RequestChannel.Response, connectionId: String): Unit
+
   def beforeSendResponseToQueue(response: RequestChannel.Response): Unit
 
   def beforeProcessResponse(response: RequestChannel.Response, connectionId: String): Unit
 
   def afterProcessResponse(response: RequestChannel.Response, connectionId: String): Unit
-
-  def beforeSendRequestToQueue(request: RequestChannel.Request, connectionId: String): Unit
-
-  def afterUnmuteChannel(response: RequestChannel.Response, connectionId: String): Unit
 }
