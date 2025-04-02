78515d158659455572eaaea712e3544c5edeb7ee
eseokhwan <huan99@hanmail.net>
2025-04-02 15:46:38 +0900

    fix: syntax error on scala

--- core/src/main/scala/kafka/interceptor/BrokerInterceptor.scala
+++ core/src/main/scala/kafka/interceptor/BrokerInterceptor.scala
@@ -4,13 +4,13 @@ import kafka.network.RequestChannel
 
 class BrokerInterceptor extends IBrokerInterceptor {
 
-  override def beforeSendRequestToQueue(request: RequestChannel.Request, connectionId: String): Unit = _
+  override def beforeSendRequestToQueue(request: RequestChannel.Request, connectionId: String): Unit = {}
 
-  override def afterUnmuteChannel(response: RequestChannel.Response, connectionId: String): Unit = _
+  override def afterUnmuteChannel(response: RequestChannel.Response, connectionId: String): Unit = {}
 
-  override def beforeSendResponseToQueue(response: RequestChannel.Response): Unit = _
+  override def beforeSendResponseToQueue(response: RequestChannel.Response): Unit = {}
 
-  override def beforeProcessResponse(response: RequestChannel.Response, connectionId: String): Unit = _
+  override def beforeProcessResponse(response: RequestChannel.Response, connectionId: String): Unit = {}
 
-  override def afterProcessResponse(response: RequestChannel.Response, connectionId: String): Unit = _
+  override def afterProcessResponse(response: RequestChannel.Response, connectionId: String): Unit = {}
 }
