4bb7c16c03404c81aa33599614729cc5e81e2f3f
eseokhwan <huan99@hanmail.net>
2025-04-02 15:46:47 +0900

    fix: syntax error on scala

--- core/src/main/scala/kafka/interceptor/ProduceRequestRateCheckInterceptor.scala
+++ core/src/main/scala/kafka/interceptor/ProduceRequestRateCheckInterceptor.scala
@@ -22,7 +22,7 @@ class ProduceRequestRateCheckInterceptor extends IBrokerInterceptor with Logging
     }
   }
 
-  override def afterUnmuteChannel(response: RequestChannel.Response, connectionId: String): Unit = _
+  override def afterUnmuteChannel(response: RequestChannel.Response, connectionId: String): Unit = {}
 
   override def beforeSendResponseToQueue(response: RequestChannel.Response): Unit = {
     if (response.request.header.apiKey() == ApiKeys.PRODUCE) {
@@ -34,7 +34,7 @@ class ProduceRequestRateCheckInterceptor extends IBrokerInterceptor with Logging
     }
   }
 
-  override def beforeProcessResponse(response: RequestChannel.Response, connectionId: String): Unit = _
+  override def beforeProcessResponse(response: RequestChannel.Response, connectionId: String): Unit = {}
 
-  override def afterProcessResponse(response: RequestChannel.Response, connectionId: String): Unit = _
+  override def afterProcessResponse(response: RequestChannel.Response, connectionId: String): Unit = {}
 }
